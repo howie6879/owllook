590b81636823f66f837a61c1902f730299580dde
howie6879 <xiaozizayang@gmail.com>
2017-02-23 17:50:09 +0800

    session

--- README.md
+++ README.md
@@ -6,10 +6,17 @@
 
 `novels-search`是一个基于其他网站的垂直小说搜索引擎，至于为什么写这个，一个是想利用`sanic`尽量做成异步服务，二是想就此练习下推荐系统。
 
-- sanic：基于Python 3.5+的异步web服务器，快快快。
+- sanic：基于Python 3.5+的异步web服务器，快快快
+- sanic_session：sanic的持续会话插件
 - vloop：sanic默认使用uvloop，替代asyncio本身的loop
 - motor：异步的mongodb驱动
+- asyncio_redis：redis异步支持
+- aiohttp：异步请求
 
+
+对于用户信息，利用mongodb进行存储。
+
+某些必要的缓存，利用redis进行缓存处理，注意，对于限制数据：都将在24小时删除。
 
 对于不同网站的小说，页面规则都不尽相同，我希望能够在代码解析后再统一展示出来，这样方便且美观，而不是仅仅跳转到对应网站就完事，清新简洁的阅读体验才是最重要的。
 
---
+++ tests/session_test.py
@@ -0,0 +1,62 @@
+import asyncio_redis
+
+from sanic import Sanic
+from sanic.response import text
+from sanic_session import RedisSessionInterface
+
+app = Sanic()
+
+
+# Token from https://github.com/subyraman/sanic_session
+
+class Redis:
+    """
+    A simple wrapper class that allows you to share a connection
+    pool across your application.
+    """
+    _pool = None
+
+    async def get_redis_pool(self):
+        if not self._pool:
+            self._pool = await asyncio_redis.Pool.create(
+                host='localhost', port=6379, poolsize=10
+            )
+
+        return self._pool
+
+
+redis = Redis()
+
+# pass the getter method for the connection pool into the session
+session_interface = RedisSessionInterface(redis.get_redis_pool, expiry=604800)
+
+
+@app.middleware('request')
+async def add_session_to_request(request):
+    # before each request initialize a session
+    # using the client's request
+    await session_interface.open(request)
+
+
+@app.middleware('response')
+async def save_session(request, response):
+    # after each request save the session,
+    # pass the response to set client cookies
+    await session_interface.save(request, response)
+
+
+@app.route("/")
+async def test(request):
+    # interact with the session like a normal dict
+    if not request['session'].get('foo'):
+        request['session']['foo'] = 0
+
+    request['session']['foo'] += 1
+
+    response = text(request['session']['foo'])
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888, debug=True)
