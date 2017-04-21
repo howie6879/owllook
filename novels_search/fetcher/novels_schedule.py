ff44b08bd9a2cb1ff15379108095aad506afafae
howie6879 <xiaozizayang@gmail.com>
2017-04-21 13:57:21 +0800

    小说每隔60min检查最新章节

---
+++ novels_search/fetcher/novels_schedule.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import asyncio
+import uvloop
+import schedule
+import time
+import sys
+
+sys.path.append('../../')
+
+from novels_search.fetcher.cache import update_all_books
+
+
+def update_all_books_schedule():
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    task = asyncio.ensure_future(update_all_books())
+    loop.run_until_complete(task)
+    return task.result()
+
+
+# python novels_schedule.py
+schedule.every(60).minutes.do(update_all_books_schedule)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
