0056b8963687a6eb67304b89ace6387018d6f19c
howie6879 <xiaozizayang@gmail.com>
2017-07-24 22:21:47 +0800

    最新章节获取增加超时机制

--- owllook/fetcher/cache.py
+++ owllook/fetcher/cache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import aiohttp
+import asyncio
 import re
+import async_timeout
 
 from bs4 import BeautifulSoup
 from aiocache.serializers import PickleSerializer
@@ -150,74 +152,83 @@ async def cache_owllook_search_ranking():
 
 
 async def get_the_latest_chapter(chapter_url):
-    url = parse_qs(urlparse(chapter_url).query).get('url', '')
-    novels_name = parse_qs(urlparse(chapter_url).query).get('novels_name', '')
-    data = None
-    if url and novels_name:
-        url = url[0]
-        novels_name = novels_name[0]
-        netloc = urlparse(url).netloc
-        if netloc in LATEST_RULES.keys():
-            async with aiohttp.ClientSession() as client:
-                try:
-                    html = await target_fetch(client=client, url=url)
-                    if html is None:
-                        html = requests_target_fetch(url=url)
-                except TypeError:
-                    html = requests_target_fetch(url=url)
-                except Exception as e:
-                    LOGGER.exception(e)
-                    return None
-                try:
-                    soup = BeautifulSoup(html, 'html5lib')
-                except Exception as e:
-                    LOGGER.exception(e)
-                    return None
-                latest_chapter_name, latest_chapter_url = None, None
-                if LATEST_RULES[netloc].plan:
-                    meta_value = LATEST_RULES[netloc].meta_value
-                    latest_chapter_name = soup.select('meta[property="{0}"]'.format(meta_value["latest_chapter_name"]))
-                    latest_chapter_name = latest_chapter_name[0].get('content', None) if latest_chapter_name else None
-                    latest_chapter_url = soup.select('meta[property="{0}"]'.format(meta_value["latest_chapter_url"]))
-                    latest_chapter_url = latest_chapter_url[0].get('content', None) if latest_chapter_url else None
-                else:
-                    selector = LATEST_RULES[netloc].selector
-                    content_url = selector.get('content_url')
-                    if selector.get('id', None):
-                        latest_chapter_soup = soup.find_all(id=selector['id'])
-                    elif selector.get('class', None):
-                        latest_chapter_soup = soup.find_all(class_=selector['class'])
-                    else:
-                        latest_chapter_soup = soup.select(selector.get('tag'))
-                    if latest_chapter_soup:
-                        if content_url == '1':
-                            # TODO
-                            pass
-                        elif content_url == '0':
-                            # TODO
-                            pass
+    try:
+        with async_timeout.timeout(20):
+            url = parse_qs(urlparse(chapter_url).query).get('url', '')
+            novels_name = parse_qs(urlparse(chapter_url).query).get('novels_name', '')
+            data = None
+            if url and novels_name:
+                url = url[0]
+                novels_name = novels_name[0]
+                netloc = urlparse(url).netloc
+                if netloc in LATEST_RULES.keys():
+                    async with aiohttp.ClientSession() as client:
+                        try:
+                            html = await target_fetch(client=client, url=url)
+                            if html is None:
+                                html = requests_target_fetch(url=url)
+                        except TypeError:
+                            html = requests_target_fetch(url=url)
+                        except Exception as e:
+                            LOGGER.exception(e)
+                            return None
+                        try:
+                            soup = BeautifulSoup(html, 'html5lib')
+                        except Exception as e:
+                            LOGGER.exception(e)
+                            return None
+                        latest_chapter_name, latest_chapter_url = None, None
+                        if LATEST_RULES[netloc].plan:
+                            meta_value = LATEST_RULES[netloc].meta_value
+                            latest_chapter_name = soup.select(
+                                'meta[property="{0}"]'.format(meta_value["latest_chapter_name"]))
+                            latest_chapter_name = latest_chapter_name[0].get('content',
+                                                                             None) if latest_chapter_name else None
+                            latest_chapter_url = soup.select(
+                                'meta[property="{0}"]'.format(meta_value["latest_chapter_url"]))
+                            latest_chapter_url = latest_chapter_url[0].get('content',
+                                                                           None) if latest_chapter_url else None
                         else:
-                            latest_chapter_url = content_url + latest_chapter_soup[0].get('href', None)
-                        latest_chapter_name = latest_chapter_soup[0].get('title', None)
-                if latest_chapter_name and latest_chapter_url:
-                    time_current = get_time()
-                    data = {
-                        "latest_chapter_name": latest_chapter_name,
-                        "latest_chapter_url": latest_chapter_url,
-                        "owllook_chapter_url": chapter_url,
-                        "owllook_content_url": "/owllook_content?url={latest_chapter_url}&name={name}&chapter_url={chapter_url}&novels_name={novels_name}".format(
-                            latest_chapter_url=latest_chapter_url,
-                            name=latest_chapter_name,
-                            chapter_url=url,
-                            novels_name=novels_name,
-                        ),
-                    }
-                    # 存储最新章节
-                    motor_db = MotorBase().db
-                    await motor_db.latest_chapter.update_one(
-                        {"novels_name": novels_name, 'owllook_chapter_url': chapter_url},
-                        {'$set': {'data': data, "finished_at": time_current}}, upsert=True)
-    return data
+                            selector = LATEST_RULES[netloc].selector
+                            content_url = selector.get('content_url')
+                            if selector.get('id', None):
+                                latest_chapter_soup = soup.find_all(id=selector['id'])
+                            elif selector.get('class', None):
+                                latest_chapter_soup = soup.find_all(class_=selector['class'])
+                            else:
+                                latest_chapter_soup = soup.select(selector.get('tag'))
+                            if latest_chapter_soup:
+                                if content_url == '1':
+                                    # TODO
+                                    pass
+                                elif content_url == '0':
+                                    # TODO
+                                    pass
+                                else:
+                                    latest_chapter_url = content_url + latest_chapter_soup[0].get('href', None)
+                                latest_chapter_name = latest_chapter_soup[0].get('title', None)
+                        if latest_chapter_name and latest_chapter_url:
+                            time_current = get_time()
+                            data = {
+                                "latest_chapter_name": latest_chapter_name,
+                                "latest_chapter_url": latest_chapter_url,
+                                "owllook_chapter_url": chapter_url,
+                                "owllook_content_url": "/owllook_content?url={latest_chapter_url}&name={name}&chapter_url={chapter_url}&novels_name={novels_name}".format(
+                                    latest_chapter_url=latest_chapter_url,
+                                    name=latest_chapter_name,
+                                    chapter_url=url,
+                                    novels_name=novels_name,
+                                ),
+                            }
+                            # 存储最新章节
+                            motor_db = MotorBase().db
+                            await motor_db.latest_chapter.update_one(
+                                {"novels_name": novels_name, 'owllook_chapter_url': chapter_url},
+                                {'$set': {'data': data, "finished_at": time_current}}, upsert=True)
+            return data
+    except Exception as e:
+        LOGGER.exception(e)
+        return None
 
 
 async def update_all_books():
@@ -225,18 +236,27 @@ async def update_all_books():
         motor_db = MotorBase().db
         # 获取所有书架链接游标
         books_url_cursor = motor_db.user_message.find({}, {'books_url.book_url': 1, '_id': 0})
-        # 已更新url集合
+        book_urls = []
         already_urls = set()
+        # url_tasks = [get_the_latest_chapter(each_url) for each_url in set(book_urls)]
+        # tasks = [asyncio.ensure_future(i) for i in url_tasks]
+        # return await asyncio.gather(*tasks)
         async for document in books_url_cursor:
             if document:
                 books_url = document['books_url']
+
                 # 一组书架链接列表数据
+                # book_urls += [book_url['book_url'] for book_url in books_url]
                 for book_url in books_url:
                     chapter_url = book_url['book_url']
                     if chapter_url not in already_urls:
-                        await get_the_latest_chapter(chapter_url)
+                        try:
+                            with async_timeout.timeout(20):
+                                await get_the_latest_chapter(chapter_url)
+                        except Exception as e:
+                            LOGGER.exception(e)
                         already_urls.add(chapter_url)
-        return True
+
     except Exception as e:
         LOGGER.exception(e)
         return False
--- owllook/fetcher/novels_schedule.py
+++ owllook/fetcher/novels_schedule.py
@@ -13,11 +13,10 @@ from owllook.fetcher.cache import update_all_books
 
 
 def update_all_books_schedule():
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    # asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
     task = asyncio.ensure_future(update_all_books())
     loop.run_until_complete(task)
-    return task.result()
 
 
 # python novels_schedule.py
