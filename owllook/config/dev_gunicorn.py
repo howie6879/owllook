5f03b0101a56d17f99f74413fbc303b227eb22b0
howie6879 <xiaozizayang@gmail.com>
2018-08-15 16:26:38 +0800

    Update gunicorn config

--- owllook/config/dev_gunicorn.py
+++ owllook/config/dev_gunicorn.py
@@ -1,18 +1,17 @@
 # gunicorn config
 import os
 
-WORKERS = os.getenv('WORKERS', 1)
+WORKERS = os.getenv('WORKERS', 2)
+TIMEOUT = os.getenv('TIMEOUT', 60)
 
-
-bind = '0.0.0.0:8001'
 backlog = 2048
 
 workers = WORKERS
 worker_connections = 1000
-timeout = 30
 keepalive = 2
 
 spew = False
 daemon = False
 umask = 0
+timeout = TIMEOUT
 preload = True
